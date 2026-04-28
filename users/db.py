bea1269c3a73ffbaf3216c72636b1bc185becb23
yordycg <yordy.gonzalez.g@gmail.com>
2026-04-28 14:34:49 -0400

    feat: implement manual SQL database manager with environment config

---
+++ users/db.py
@@ -0,0 +1,59 @@
+import pymysql
+from django.conf import settings
+
+
+class DatabaseManager:
+    def get_connection(self):
+        return pymysql.connect(
+            host=settings.DB_CONFIG["HOST"],
+            user=settings.DB_CONFIG["USER"],
+            password=settings.DB_CONFIG["PASSWORD"],
+            database=settings.DB_CONFIG["NAME"],
+            port=settings.DB_CONFIG["PORT"],
+            # Mecanismo que permite ir fila x fila...
+            # DictCursor: retorna los datos como diccionarios de python.
+            cursorclass=pymysql.cursors.DictCursor,
+        )
+
+    def execute(self, query, params=None):
+        """Ejecutar una sola query."""
+        # Conectarse a la db de mysql
+        connection = self.get_connection()
+        cursor = connection.cursor()
+
+        # Procedimiento dentro de la db...
+        cursor.execute(query, params)
+        connection.commit()
+
+        # Cerrar conexion de mysql
+        cursor.close()
+        connection.close()
+
+    def exists(self, query, params=None):
+        """Verificar si un registro existe."""
+        connection = self.get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(query, params)
+        # Otener solo la 1era fila, y ya saber si existe un registro asociado.
+        result = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        # Retornar el resultado sino esta vacio.
+        return result is not None
+
+    def get_all(self, query, params=None):
+        """Retorna todas la filas."""
+        connection = self.get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(query, params)
+        # Obtener todos los datos...
+        result = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        return result
