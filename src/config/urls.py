ea651de822054620874733b2828166769b877e23
Yordy Carmona <yordy.carmona@virginiogomez.cl>
2026-06-02 15:43:35 -0400

    feat: configure root redirect to login view

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='authentication:login')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('auth/', include('authentication.urls')),
